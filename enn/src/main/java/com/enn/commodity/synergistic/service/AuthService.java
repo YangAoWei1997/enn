1ec4d4e27ec3a53de7c86b7f3d40ed54a3b199d5
YangAoWei1997 <18698034263@163.com>
2019-05-16 14:06:38 +0800

    del

--- enn/src/main/java/com/enn/commodity/synergistic/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.enn.commodity.synergistic.service;
-
-import net.sf.json.JSONObject;
-
-public interface AuthService {
-	
-	public String CreateAuth(JSONObject requestJson);
-
-}
--- enn/src/main/java/com/mchain/apollo/common/entity/base/FormatType.java
+++
@@ -1,6 +0,0 @@
-package com.mchain.apollo.common.entity.base;
-
-public class FormatType {
-	public final static String STRING = "JSONSTRING";
-	public final static String OBJECT = "OBJECT";
-}
